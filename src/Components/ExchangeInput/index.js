c9845016f28072513884e9a72e4a4111502c79f8
Azhar Ali <azharch.140@gmail.com>
2021-10-09 01:46:56 +0500

    updates balnce sheet screen

--- src/Components/ExchangeInput/index.js
+++ src/Components/ExchangeInput/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import React from 'react';
 import {View, Text, TextInput, Image} from 'react-native';
 import styles from './style';
--- src/Components/ExchangeInput/style.js
+++ src/Components/ExchangeInput/style.js
@@ -1,46 +1,53 @@
-import { StyleSheet } from 'react-native'
+import {StyleSheet} from 'react-native';
+import {FontFamilyMedium} from '../../Utils/AppContants';
 
 export default StyleSheet.create({
-    container:{
-        marginTop:20
-    },
-    label:{
-        fontSize:14,
-        marginBottom:20
-    },
-    row:{
-        flexDirection:'row'
-    },
-    inputField:{
-        width:'38%',
-        height:50,
-        backgroundColor:'white',
-        borderWidth:1,
-        borderColor:'black'
-    },
-    textField:{
-        width:'38%',
-        height:50,
-        backgroundColor:'white',
-        borderWidth:1,
-        borderColor:'black',
-        padding:4,
-        paddingTop:15
-    },
-    iconContainer:{
-        width:'12%', 
-        backgroundColor:'white', 
-        alignSelf:'center', 
-        alignItems:'center', 
-        borderWidth:0.5, 
-        borderColor:'black',
-        height:50
-    },
-    centerIcon:{
-        width:30,
-        height:30,
-        alignItems:'center',
-        alignSelf:'center', 
-        marginTop:10
-    }
-})
+  container: {
+    marginTop: 20,
+  },
+  label: {
+    fontSize: 14,
+    marginBottom: 20,
+    fontFamily: FontFamilyMedium,
+  },
+  row: {
+    flexDirection: 'row',
+    borderRadius: 100,
+  },
+  inputField: {
+    width: '38%',
+    height: 50,
+    // backgroundColor: 'white',
+    borderWidth: 0.5,
+    borderColor: 'white',
+    borderTopLeftRadius: 100,
+    borderBottomLeftRadius: 100,
+    paddingLeft: 10,
+  },
+  textField: {
+    width: '38%',
+    height: 50,
+    backgroundColor: 'white',
+    borderWidth: 1,
+    borderColor: 'black',
+    padding: 4,
+    paddingTop: 15,
+  },
+  iconContainer: {
+    width: '12%',
+    // backgroundColor: 'white',
+    alignSelf: 'center',
+    alignItems: 'center',
+    borderTopWidth: 0.5,
+    borderBottomWidth: 1,
+    borderColor: 'white',
+    height: 50,
+  },
+  centerIcon: {
+    width: 30,
+    height: 30,
+    alignItems: 'center',
+    alignSelf: 'center',
+    marginTop: 10,
+  },
+});
